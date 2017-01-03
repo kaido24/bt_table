741fc8650c327596cb6bb156daa0e4fa4d01b214
David <Endzakiel@users.noreply.github.com>
2017-01-03 15:30:47 +0100

    Update fr.js

--- lang/fr.js
+++ lang/fr.js
@@ -1,6 +1,6 @@
 "use strict"
 CKEDITOR.plugins.setLang( 'bt_table', 'fr', {
-  "compactStyle": "Style compact",
+  "compactStyle": "Style compacte",
   "addBorders": "Ajouter des bordures",
   "addStripes": "Ajouter des rayures",
   "addHover": "Ajouter un effet de survol",
